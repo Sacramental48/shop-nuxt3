346d070bdc94f659d7125b8cba54ab664ea857b0
Aleksey <lomberjos33hg@gmail.com>
2024-01-19 11:41:10 +0300

    implementation footer component

---
+++ components/MainComponents/FooterItem.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const usefulLinks = ref(['CONTACT', 'TERMS OF SERVICES', 'SHIPPING AND RETURNS']);
+const footerIconsLink = ref([
+    {path: '/svg-footer/in-icon.svg', alt: 'in'},
+    {path: '/svg-footer/facebook-icon.svg', alt: 'facebook'},
+    {path: '/svg-footer/instagram-icon.svg', alt: 'instagram'},
+    {path: '/svg-footer/twitterX-icon.svg', alt: 'twitterX'}
+])
+</script>
+
+<template>
+    <div class="footer">
+        <div class="footer__useful">
+            <div class="footer__useful-block">
+                <NuxtLink class="footer__useful-item" v-for="item in usefulLinks" to="">{{ item }}</NuxtLink>
+            </div>
+            <div class="footer__useful-input">
+                <input type="text" placeholder="Give an email, get the newsletter.">
+                <button><img src="/right-arrow-icon.svg" alt=""></button>
+            </div>
+        </div>
+        <div class="footer__lower">
+            <p class="footer__privacy">© 2021 Shelly. <span>Terms of use</span> and <span>privacy policy.</span></p>
+            <div class="footer__icons">
+                <NuxtLink class="footer__icons-link" v-for="item in footerIconsLink" :key="item.path" to="">
+                    <img :src="`${item.path}`" :alt="`${item.alt}`">
+                </NuxtLink>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped lang="scss">
+@import './styles/FooterStyles.scss'
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,9 @@
     <div class="wrapper">
         <MainComponentsHeaderItem />
         <main>
-
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt distinctio iste quidem error, nobis repudiandae aspernatur amet accusantium fuga obcaecati libero consequatur veniam eligendi, et odio recusandae nisi sequi ea quod nostrum assumenda odit consequuntur. Voluptate beatae sapiente minima, eligendi accusantium facilis. Asperiores similique nihil ipsa? Distinctio ea laudantium, placeat commodi voluptate earum quis repellendus cum qui dolore aliquam reiciendis quae eius eos, sunt minus, facilis consequatur? Dolorum, aliquam ipsam optio, veritatis quos distinctio tenetur blanditiis officiis libero quia quaerat numquam sit praesentium magnam possimus est. Eveniet porro ullam optio ratione ea enim veritatis sunt assumenda autem, rerum expedita debitis.
         </main>
-        <footer>
-
-        </footer>
+        <MainComponentsFooterItem />
     </div>
 </template>
 <style scoped lang="scss">
