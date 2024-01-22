fdd05a9d87ae9d051d8640757593e0221ea5a579
Aleksey <lomberjos33hg@gmail.com>
2024-01-22 10:18:33 +0300

    create User login page

---
+++ pages/UserItem.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="user-account">
+        <span class="user-account__title">My account</span>
+        <div class="user-account__buttons">
+            <button class="user-account__signin"></button>
+            <button class="user-account__register"></button>
+        </div>
+        <form action="" class="user-account__form">
+            <input class="form__email" type="text" name="email" placeholder="Email">
+            <input class="form__password" type="text" name="password" placeholder="Password">
+            <input class="form__checkbox" type="checkbox" name="checkbox" value="">
+            <button class="form__signin">SIGN IN</button>
+            <NuxtLink class="form__forgot-password">Have you forgotten your password?</NuxtLink>
+        </form>
+    </div>
+</template>
+<style scoped lang="scss">
+@import '../assets/scss/user/UserStyle.scss';
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,16 +4,22 @@
 
 <template>
     <div class="wrapper">
-        <MainComponentsHeaderItem />
-        <main class="main">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt distinctio iste quidem error, nobis repudiandae aspernatur amet accusantium fuga obcaecati libero consequatur veniam eligendi, et odio recusandae nisi sequi ea quod nostrum assumenda odit consequuntur. Voluptate beatae sapiente minima, eligendi accusantium facilis. Asperiores similique nihil ipsa? Distinctio ea laudantium, placeat commodi voluptate earum quis repellendus cum qui dolore aliquam reiciendis quae eius eos, sunt minus, facilis consequatur? Dolorum, aliquam ipsam optio, veritatis quos distinctio tenetur blanditiis officiis libero quia quaerat numquam sit praesentium magnam possimus est. Eveniet porro ullam optio ratione ea enim veritatis sunt assumenda autem, rerum expedita debitis.
-        </main>
-        <MainComponentsFooterItem />
+        <div class="container">
+            <MainComponentsHeaderItem />
+            <main class="main">
+                <NuxtLoadingIndicator />
+                <component />
+            </main>
+            <MainComponentsFooterItem />
+        </div>
     </div>
 </template>
 <style scoped lang="scss">
 .wrapper {
     max-width: 1248px;
     margin: 0 auto;
+}
+.container {
+    // margin: 0 96px;
 }
 </style>
