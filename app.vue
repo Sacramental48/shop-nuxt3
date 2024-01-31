077e093a9acc103165b727fe4799b0e2ba82bdfc
Aleksey <lomberjos33hg@gmail.com>
2024-01-31 08:38:15 +0300

    changed styles in app

--- app.vue
+++ app.vue
@@ -1,27 +1,38 @@
 <template>
-    <div>
-        <div class="wrapper">
-            <div class="container">
-                <MainComponentsHeaderItem />
-                <main class="main">
-                    <NuxtLoadingIndicator />
-                    <NuxtPage />
-                </main>
-                <MainComponentsFooterItem />
-            </div>
-        </div>
+    <div class="wrapper">
+        <MainComponentsHeaderItem />
+        <main class="wrapper__main">
+            <NuxtLoadingIndicator />
+            <NuxtPage />
+        </main>
+        <MainComponentsFooterItem />
     </div>
 </template>
 <script setup lang="ts">
 </script>
 <style scoped lang="scss">
 .wrapper {
-    max-width: 1248px;
-    margin: 0 auto;
-}
+    @media (min-width: 575.98px){
+        max-width: 540px;
+        margin: auto;
+        padding: 0;
+    }
 
-.main {
-    display: flex;
-    justify-content: center;
+    @media (min-width: 767.98px){
+        max-width: 720px;	
+    }
+
+    @media (min-width: 991.98px) {
+        max-width: 960px;
+    }
+
+    @media (min-width: 1199.98px) {
+        max-width: 1248px;
+    }
+
+    &__main {
+        display: flex;
+        justify-content: center;
+    }
 }
 </style>
